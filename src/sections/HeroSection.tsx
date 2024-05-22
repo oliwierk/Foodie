75e211d47116463236fb5dee24e05f0a60d5561a
oliwierk <oliwier.kotlicki@gmail.com>
2024-05-22 15:16:36 +0200

    mobile view added

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -1,26 +1,23 @@
 const HeroSection: React.FC = () => {
 	return (
-		<div className='relative flex flex-col xl:flex-row h-[100vh] w-full'>
+		<div className='relative flex flex-wrap h-[100vh] w-full'>
 			<h1 className='absolute top-4 xl:top-0 left-4 xl:left-10 text-8xl xl:text-[24rem] font-bold uppercase tracking-tighter text-rose-500 z-10'>
 				foodie
 			</h1>
 			<div className='relative flex flex-row w-full h-1/2 xl:w-1/2 xl:h-full bg-orange-100'>
-				<div className='w-1/2'></div>
-				<div className='relative w-1/2'>
-					<p className='absolute bottom-40 right-8'>
-						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam
-						nisi quod at dignissimos explicabo quam veniam nostrum deleniti,
-						delectus modi!
-					</p>
-					<a
-						href=''
-						className='text-7xl absolute bottom-14 left-0 uppercase underline text-black hover:blur-sm transition-all duration-300'
-					>
-						See menu
-					</a>
-				</div>
+				<p className='absolute bottom-1/3 xl:bottom-40 left-4 xl:left-1/2 text-sm xl:text-md text-rose-400 mr-4 lg:mr-0'>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam nisi
+					quod at dignissimos explicabo quam veniam nostrum deleniti, delectus
+					modi!
+				</p>
+				<a
+					href=''
+					className='text-4xl xl:text-7xl absolute bottom-10 xl:bottom-14 left-1/2 uppercase underline hover:blur-sm transition-all duration-300 text-rose-500'
+				>
+					See menu
+				</a>
 			</div>
-			<div className='w-full h-1/2 xl:w-1/2 xl:h-full bg-herobg bg-cover blur-sm hover:blur-0 transition-all duration-200'></div>
+			<div className='w-full h-1/2 xl:w-1/2 xl:h-full bg-herobg bg-cover transition-all duration-200'></div>
 		</div>
 	);
 };
--- src/sections/MenuChoice.tsx
+++ src/sections/MenuChoice.tsx
@@ -1,21 +1,21 @@
 const MenuChoice: React.FC = () => {
 	return (
 		<div className='relative flex flex-wrap h-[150vh] w-full'>
-			<div className='relative flex items-center justify-center w-1/2 h-1/2 bg-pizza bg-cover bg-top'>
+			<div className='relative flex items-center justify-center w-full xl:w-1/2 h-1/2 bg-pizza bg-cover bg-top'>
 				<div className='absolute inset-0 bg-yellow-300 opacity-0 hover:opacity-100 transition-opacity duration-300'></div>
-				<p className='text-8xl uppercase z-10'>Pizza's</p>
+				<p className='text-7xl xl:text-9xl uppercase z-10'>Pizza's</p>
 			</div>
-			<div className='relative flex items-center justify-center w-1/2 h-1/2 bg-burger bg-cover bg-center'>
+			<div className='relative flex items-center justify-center w-full xl:w-1/2 h-1/2 bg-burger bg-cover bg-center'>
 				<div className='absolute inset-0 bg-pink-300 opacity-0 hover:opacity-100 transition-opacity duration-300'></div>
-				<p className='text-8xl uppercase z-10'>Burgers</p>
+				<p className='text-7xl xl:text-9xl uppercase z-10'>Burgers</p>
 			</div>
-			<div className='relative flex items-center justify-center w-1/2 h-1/2 bg-drink bg-cover bg-center'>
+			<div className='relative flex items-center justify-center w-full xl:w-1/2 h-1/2 bg-drink bg-cover bg-center'>
 				<div className='absolute inset-0 bg-indigo-300 opacity-0 hover:opacity-100 transition-opacity duration-300'></div>
-				<p className='text-8xl uppercase z-10'>Drinks</p>
+				<p className='text-7xl xl:text-9xl uppercase z-10'>Drinks</p>
 			</div>
-			<div className='relative flex items-center justify-center w-1/2 h-1/2 bg-taco bg-cover bg-center'>
+			<div className='relative flex items-center justify-center w-full xl:w-1/2 h-1/2 bg-taco bg-cover bg-center'>
 				<div className='absolute inset-0 bg-indigo-600 opacity-0 hover:opacity-100 transition-opacity duration-300'></div>
-				<p className='text-8xl uppercase z-10'>Tacos</p>
+				<p className='text-7xl xl:text-9xl uppercase z-10'>Tacos</p>
 			</div>
 		</div>
 	);
